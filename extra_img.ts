f7766ded89ca9ada5c6e857e4b01d886257d53a4
antonilol <49868160+antonilol@users.noreply.github.com>
2019-09-29 13:50:22 +0200

    Import from antonilol/test

---
+++ extra_img.ts
@@ -0,0 +1,270 @@
+//% color="#007FFF" block="(Extra) Images" weight=100 icon="\uf03e"
+namespace extra_img {
+    export class Anim {
+        frames: Image[];
+        //% block="show $myAnim" parts="Anim"
+        show(): void {
+
+        }
+
+    }
+    export enum Operator {
+        //% block="a and b"
+        and = 0,
+        //% block="not (a and b)"
+        nand = 2,
+        //% block="a or b"
+        or = 1,
+        //% block="not (a or b)"
+        nor = 3,
+        //% block="a xor b"
+        xor = 4,
+        //% block="not (a xor b)"
+        xnor = 5,
+        //% block="not a"
+        inv1 = 6,
+        //% block="not b"
+        inv2 = 7,
+        //% block="a - b"
+        min1 = 8,
+        //% block="b - a"
+        min2 = 9,
+    }
+    export enum Position {
+        //% block="left"
+        left = 0,
+        //% block="centre"
+        centre = 1,
+        //% block="right"
+        right = 2
+    }
+    /**
+     * TODO [DEFAULT]
+     */
+    //% block="DELETED BLOCK" blockId=newPoint weight=91
+    export function newPoint(): void {
+
+
+    }
+
+    /**
+     * TODO
+     */
+    //% block="empty" blockId=empty weight=99
+    export function empty(): Image {
+        return images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `);
+    }
+    /**
+     * TODO
+     */
+    //% block="filled" blockId=filled  weight=97
+    export function filled(): Image {
+        return images.createImage(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `);
+    }
+    /**
+     * TODO
+     */
+    //% block="random" blockId=random  weight=98
+    export function random(): Image {
+        let output: Image = null
+        output = images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+        for (let imgx = 0; imgx < 5; imgx++) {
+            for (let imgy = 0; imgy < 5; imgy++) {
+                output.setPixel(imgx, imgy, Math.randomBoolean())
+            }
+        }
+        return output
+    }
+
+    /**
+     * Boolean operators for images
+     * @param imgA eg:null
+     * @param imgB eg:null
+     */
+    //% x.fieldEditor="gridpicker" x.fieldOptions.columns=2
+    //% x.fieldOptions.tooltips="false" 
+    //% block="(a = $imgA, b = $imgB) $x" blockId=opImg  weight=96
+    export function opImg(imgA: Image, imgB: Image, x: Operator): Image {
+        let output: Image = null
+        output = images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `);
+        if ((imgA && imgB) || (x == 6 && imgA) || (x == 7 && imgB)) {
+            let a: boolean = false;
+            let b: boolean = false;
+            for (let imgx = 0; imgx < 5; imgx++) {
+                for (let imgy = 0; imgy < 5; imgy++) {
+                    a = imgA.pixel(imgy, imgx);
+                    b = imgB.pixel(imgy, imgx);
+                    if (x == 0) {
+                        output.setPixel(imgy, imgx, a && b);
+                    } else if (x == 1) {
+                        output.setPixel(imgy, imgx, a || b);
+                    } else if (x == 2) {
+                        output.setPixel(imgy, imgx, !(a && b));
+                    } else if (x == 3) {
+                        output.setPixel(imgy, imgx, !(a || b));
+                    } else if (x == 4) {
+                        output.setPixel(imgy, imgx, a != b);
+                    } else if (x == 5) {
+                        output.setPixel(imgy, imgx, a == b);
+                    } else if (x == 6) {
+                        output.setPixel(imgy, imgx, !a);
+                    } else if (x == 7) {
+                        output.setPixel(imgy, imgx, !b);
+                    } else if (x == 8) {
+                        output.setPixel(imgy, imgx, a && (a != b));
+                    } else if (x == 9) {
+                        output.setPixel(imgy, imgx, b && (a != b));
+                    } else {
+                        output.setPixel(imgy, imgx, false);
+                    }
+                }
+            }
+        }
+        return output;
+    }
+
+    /**
+     * Shows the given image
+     * @param img eg:null
+     */
+    //% block="show image $img" blockId=showImage weight=100
+    export function showImg(img: Image): void {
+        if (img) {
+            img.showImage(0)
+        }
+    }
+
+    /**
+     * TODO
+     */
+    //% block="binary (number $numb) (+$offset|px) (reversed $reversed)" blockId=binImage  weight=95
+    export function binImage(numb: number, offset: number, reversed: boolean): Image {
+        let output: Image = null
+        output = images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+        for (let index = 0; index <= 24; index++) {
+            if (numb >= 2 ** (24 - index)) {
+                numb += -1 * 2 ** (24 - index)
+                if (reversed) {
+                    output.setPixel((offset - index) % 5, Math.floor((offset - index) / 5), true)
+                } else {
+                    output.setPixel((offset + 24 - index) % 5, Math.floor((offset + 24 - index) / 5), true)
+                }
+            }
+        }
+        return output
+    }
+    /**
+     * Edit a particular pixel on an image
+     * @param img eg:null
+     * @param point eg:extra_img.newPoint(0,0)
+     */
+    //% block="edit pixel on image $img x $x y $y to $newvalue" blockId=editImage  weight=94
+    export function editImage(img: Image, x: number, y: number, newvalue: boolean): void {
+        if (img) {
+            img.setPixel(x, y, newvalue)
+        }
+    }
+
+    /**
+     * Returns an image with a 7-segment 1-digit number.
+     * @param offset eg:1
+     */
+    //% block="7-seg <10 >=0 $value $pos" blockId=seg7
+    //% value.min=0 value.max=9 weight=93
+    //% pos.fieldEditor="gridpicker" pos.fieldOptions.columns=1
+    //% pos.fieldOptions.tooltips="false" pos.fieldOptions.width="50"
+    export function seg7(value: number, pos: Position): Image {
+        let output: Image = null
+        output = images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+        if (value >= 0 && value <= 9) {
+            let list: number[][] = [[[1, 1, 1], [1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 1, 1]],
+            [[0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1]],
+            [[1, 1, 1], [0, 0, 1], [1, 1, 1], [1, 0, 0], [1, 1, 1]],
+            [[1, 1, 1], [0, 0, 1], [1, 1, 1], [0, 0, 1], [1, 1, 1]],
+            [[1, 0, 1], [1, 0, 1], [1, 1, 1], [0, 0, 1], [0, 0, 1]],
+            [[1, 1, 1], [1, 0, 0], [1, 1, 1], [0, 0, 1], [1, 1, 1]],
+            [[1, 1, 1], [1, 0, 0], [1, 1, 1], [1, 0, 1], [1, 1, 1]],
+            [[1, 1, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1]],
+            [[1, 1, 1], [1, 0, 1], [1, 1, 1], [1, 0, 1], [1, 1, 1]],
+            [[1, 1, 1], [1, 0, 1], [1, 1, 1], [0, 0, 1], [1, 1, 1]]][value];
+            for (let y = 0; y <= 4; y++) {
+                for (let x = 0; x <= 2; x++) {
+                    if (list[y][x]) {
+                        output.setPixel(x + pos, y, true);
+                    }
+                }
+            }
+        }
+        return output
+    }
+    /**
+     * Returns an image with a 7-segment 2-digit number.
+     * @param value eg:10
+     */
+    //% block="7-seg <20 >=10 $value" blockId=seg72  weight=92
+    //% value.min=10 value.max=19
+    export function seg72(value: number): Image {
+        let output: Image = null
+        output = images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+        if (value >= 10 && value <= 19) {
+            for (let y = 0; y <= 4; y++) {
+                output.setPixel(0, y, true);
+            }
+            output = opImg(output, seg7(value - 10, 2), Operator.or);
+        }
+        return output
+    }
+    /**
+     * Creates a new animation
+     */
+    //% block="new animation" blockId=newAnim  weight=101
+    //% blockSetVariable=myAnim
+    export function newAnim(): Anim {
+        let output = new Anim;
+        output.frames = []
+        return output;
+    }
+
+}
--- main.ts
+++ main.ts
@@ -1,7 +1,276 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% color="#007FFF" block="(Extra) Functions" weight=100 icon="+"
+namespace extra {
+
+    export enum Bool {
+        and = 0,
+        or = 1,
+        nand = 2,
+        nor = 3,
+        xor = 4,
+        xnor = 5
+    }
+    export enum AnalogReadPins {
+        P0 = AnalogPin.P0,
+        P1 = AnalogPin.P1,
+        P2 = AnalogPin.P2,
+        P3 = AnalogPin.P3,
+        P4 = AnalogPin.P4,
+        P10 = AnalogPin.P10
+    }
+
+    export enum Slot {
+        //% block="1"
+        s1 = 0,
+        //% block="2"
+        s2 = 1,
+        //% block="3"
+        s3 = 2,
+        //% block="4"
+        s4 = 3,
+        //% block="5"
+        s5 = 4,
+        //% block="6"
+        s6 = 5,
+        //% block="7"
+        s7 = 6,
+        //% block="8"
+        s8 = 7,
+        //% block="9"
+        s9 = 8,
+        //% block="10"
+        s10 = 9,
+        //% block="11"
+        s11 = 10,
+        //% block="12"
+        s12 = 11,
+        //% block="13"
+        s13 = 12,
+        //% block="14"
+        s14 = 13,
+        //% block="15"
+        s15 = 14,
+        //% block="16"
+        s16 = 15,
+        //% block="17"
+        s17 = 16,
+        //% block="18"
+        s18 = 17,
+        //% block="19"
+        s19 = 18,
+        //% block="20"
+        s20 = 19,
+        //% block="21"
+        s21 = 20,
+        //% block="22"
+        s22 = 21,
+        //% block="23"
+        s23 = 22,
+        //% block="24"
+        s24 = 23,
+        //% block="25"
+        s25 = 24,
+        //% block="26"
+        s26 = 25,
+        //% block="27"
+        s27 = 26,
+        //% block="28"
+        s28 = 27,
+        //% block="29"
+        s29 = 28,
+        //% block="30"
+        s30 = 29,
+        //% block="31"
+        s31 = 30,
+        //% block="32"
+        s32 = 31,
+        //% block="33"
+        s33 = 32,
+        //% block="34"
+        s34 = 33,
+        //% block="35"
+        s35 = 34,
+        //% block="36"
+        s36 = 35,
+        //% block="37"
+        s37 = 36,
+        //% block="38"
+        s38 = 37,
+        //% block="39"
+        s39 = 38,
+        //% block="40"
+        s40 = 39,
+        //% block="41"
+        s41 = 40,
+        //% block="42"
+        s42 = 41,
+        //% block="43"
+        s43 = 42,
+        //% block="44"
+        s44 = 43,
+        //% block="45"
+        s45 = 44,
+        //% block="46"
+        s46 = 45,
+        //% block="47"
+        s47 = 46,
+        //% block="48"
+        s48 = 47,
+        //% block="49"
+        s49 = 48,
+        //% block="50"
+        s50 = 49,
+        //% block="51"
+        s51 = 50,
+        //% block="52"
+        s52 = 51,
+        //% block="53"
+        s53 = 52,
+        //% block="54"
+        s54 = 53,
+        //% block="55"
+        s55 = 54,
+        //% block="56"
+        s56 = 55,
+        //% block="57"
+        s57 = 56,
+        //% block="58"
+        s58 = 57,
+        //% block="59"
+        s59 = 58,
+        //% block="60"
+        s60 = 59,
+        //% block="61"
+        s61 = 60,
+        //% block="62"
+        s62 = 61,
+        //% block="63"
+        s63 = 62,
+        //% block="64"
+        s64 = 63
+    }
+    /**
+     * TODO [DEFAULT]
+     */
+    //% block="every $ms ms, start directly $startDirectly" blockId=repeat
+    export function repeat(ms: number, startDirectly: boolean, handler: (indexNum: number, totalWaitingTime: number) => void) {
+        let indexNum: number = 1;
+        let totalWaitingTime: number = 0;
+        if (ms > 0) {
+
+            while (true) {
+                if (!(startDirectly && indexNum == 1)) {
+                    basic.pause(ms);
+                    totalWaitingTime = totalWaitingTime + ms
+                }
+                handler(indexNum, totalWaitingTime);
+                indexNum = indexNum + 1;
+            }
+        }
+    }
+    /**
+     * On analog pin change
+     * When 'minDiff' is set to 0 or negative, this block does the same as basic.forever
+     * @param minDiff eg:1
+     */
+    //% pin.fieldEditor="gridpicker" pin.fieldOptions.columns=2
+    //% pin.fieldOptions.tooltips="false" pin.fieldOptions.width="200"
+    //% block="on analog pin change $pin minimum difference $minDiff" blockId=onAnalogPinChange
+    //% minDiff.min=0 minDiff.max=1023
+    export function onAnalogPinChange(pin: AnalogReadPins, minDiff: number, handler: (pin_value: number) => void) {
+        let analogValue1 = 0
+        let analogValue2 = 0
+        let if1 = 0
+        let pin2: number = pin
+        while (true) {
+            analogValue1 = pins.analogReadPin(pin2)
+            if (if1 && Math.abs(analogValue1 - analogValue2) >= minDiff) {
+                handler(analogValue1)
+            }
+            basic.pause(1)
+            analogValue2 = pins.analogReadPin(pin2)
+            if1 = 1
+            if (if1 && Math.abs(analogValue1 - analogValue2) >= minDiff) {
+                handler(analogValue2)
+            }
+            basic.pause(1)
+        }
+    }
+
+
+
+
+    let func2: ((x: number) => void)[] = [function (x: number) { }];
+    for (let _i = 0; _i <= 63; _i++) {
+        func2.push(function (x: number) { })
+    }
+
+
+    /**
+     * TODO [DEFAULT]
+     */
+    //% slot.fieldEditor="gridpicker" slot.fieldOptions.columns=8
+    //% slot.fieldOptions.tooltips="false" slot.fieldOptions.width="200"
+    //% block="define function with number arg (slot=$slot)" blockId=defFunction
+    export function defFunction(slot: Slot, handler: (arg: number) => void) {
+        let arg: number;
+
+        func2[slot] = handler;
+
+    }
+    /**
+     * TODO [DEFAULT]
+     */
+    //% slot.fieldEditor="gridpicker" slot.fieldOptions.columns=8
+    //% slot.fieldOptions.tooltips="false" slot.fieldOptions.width="200"
+    //% block="call function with number arg $arg (slot=$slot)" blockId=callFunction
+    export function callFunction(slot: Slot, arg: number) {
+
+        func2[slot](arg);
+
+    }
+
+
+
+    /**
+     * TODO [DEFAULT]
+     */
+    //% block="$a $x $b" blockId=op
+    export function op(a: boolean, x: Bool, b: boolean): boolean {
+        if (x == 0) {
+            return (a && b);
+        } else if (x == 1) {
+            return (a || b);
+        } else if (x == 2) {
+            return !(a && b);
+        } else if (x == 3) {
+            return !(a || b);
+        } else if (x == 4) {
+            return a != b;
+        } else if (x == 5) {
+            return a == b;
+        } else {
+            return false;
+        }
+    }
+
+
+
+
+
+    /**
+     * Stackable block to create extremely large blocks.
+     * @param value eg:3
+     */
+    //% block="$value" blockId=numb
+    export function numb(value: number): number {
+        return value;
+    }
+
+    /**
+     * Empty event block
+     */
+    //% block="" blockId=emptyevent
+    export function emptyevent(handler: () => void): void {
+
+    }
+}
